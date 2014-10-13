d4e5ed0b56bdd48a6e240a686e7ecd925245362f
Christian Lang <clang@student.neumont.edu>
2014-10-13 12:47:43 -0600

    re-added gitignore files

--- Lingual/Lingual/TranslationUtility.cs
+++ Lingual/Lingual/TranslationUtility.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace Lingual
 {
-    public class TranslationUtility
-    {
-    }
+	public class TranslationUtility : Translator
+	{
+
+		private static File _translationFile;
+
+		public static File TranslationFile
+		{
+			get
+			{
+				
+			}
+		}
+
+		public string translate(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string translate(string key, string locale)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string translate(string key, params string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string translate(string key, string locale, params string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
