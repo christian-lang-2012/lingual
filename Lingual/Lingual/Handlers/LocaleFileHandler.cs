96b1e519ea6c14963663e634ba4454bd31fe5fa1
I18N <cnuramirez@hotmail.com>
2014-10-13 15:17:30 -0600

    Added functionality to get Locale file

---
+++ Lingual/Lingual/Handlers/LocaleFileHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lingual.Handlers
+{
+    public class LocaleFileHandler
+    {
+        private String projPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+        public JObject GetLocaleFile(String localeCode)
+        {
+            var localePath = Path.Combine(projPath, "locale", localeCode + ".json");
+            var JSONLocaleFile = JObject.Parse(File.ReadAllText(localePath));
+            return JSONLocaleFile;
+        }
+
+        /// <summary>
+        /// Checks to see if the locale folder exists.
+        /// Creates the directory if it does not.
+        /// </summary>
+        /// <param name="localeFolderPath"></param>
+        private void LocaleFolderCheck(String localeFolderPath)
+        {
+            if (!Directory.Exists(localeFolderPath))
+            {
+                Directory.CreateDirectory(localeFolderPath);
+            }
+        }
+    }
+}
