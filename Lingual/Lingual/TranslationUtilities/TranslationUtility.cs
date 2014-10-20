662579eb73148617893074239c7e1c45295a87d5
c.Ram <cnuramirez@hotmail.com>
2014-10-20 13:13:23 -0600

    Pluralization, Date formating working

--- Lingual/Lingual/TranslationUtilities/TranslationUtility.cs
+++ Lingual/Lingual/TranslationUtilities/TranslationUtility.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Lingual.Handlers;
 using Lingual.Enums;
 using Newtonsoft.Json.Linq;
+using System.Globalization;
 
 namespace Lingual.TranslationUtilities
 {
@@ -79,11 +80,46 @@ namespace Lingual.TranslationUtilities
 			{
 				requestedTranslationDictionary = translationDictionary;
 			}
-			return requestedTranslationDictionary.GetValue(key, arguments);
+            var interpolStringVal = requestedTranslationDictionary.GetValue(key);
+            if (arguments.Any())
+            {
+                interpolStringVal = string.Format(interpolStringVal, arguments);
+            }
+            return interpolStringVal;
 		}
 		#endregion
 
+        /// <summary>
+        /// Returns a locale formatted string of the specified Datetime object
+        /// </summary>
+        /// <param name="key">The DateTime to localize</param>
+        /// <param name="locale">The locale</param>
+        /// <returns></returns>
+        public string TranslateDate(DateTime key, LocaleEnum locale = LocaleEnum.EN)
+        {
+            CultureInfo culture = new CultureInfo(locale.ToString());
+            return key.ToString("d", culture);
+        }
 
+        public string TranslatePlural(string key, String pluralDegree, LocaleEnum locale = LocaleEnum.EN)
+        {
+            var requestedDictionary = _translationDictionaries[0];
+            foreach (var translationDictionary in _translationDictionaries.Where(translationDictionary => translationDictionary.TranslationLocale == locale))
+            {
+                requestedDictionary = translationDictionary;
+            }
+            return requestedDictionary.GetValue(key, pluralDegree);
+        }
+
+        public string TranslatePlural(string key, String pluralDegree, LocaleEnum locale, params string[] arguments)
+        {
+            var interpolStringVal = TranslatePlural(key, pluralDegree, locale);
+            if (arguments.Any())
+            {
+                interpolStringVal = string.Format(interpolStringVal, arguments);
+            }
+            return interpolStringVal;
+        }
 		#region Helper Methods
 		/// <summary>
 		/// Sets up translation hashes.
