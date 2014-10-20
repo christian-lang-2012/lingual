13f43a93ae2958b5147564e57ada97bcdedbcb37
Christian Lang <clang@student.neumont.edu>
2014-10-20 14:08:16 -0600

    added the nunit framework, and added a simple hello key translation

---
+++ Lingual/Lingual/Testing/TranslationUtilityTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lingual.Enums;
+using Lingual.TranslationUtilities;
+using NUnit.Framework;
+
+namespace Lingual.Testing
+{
+	public class TranslationUtilityTest
+	{
+		private TranslationUtility translator = TranslationUtility.Instance;
+
+		[TestCase("login.hello")]
+		public void TestHelloKeyTranslation(string key)
+		{
+			Assert.AreEqual(translator.Translate(key), "Hello!", "The English translation failed for this key: " + key);
+			Assert.AreEqual(translator.Translate(key, LocaleEnum.ES), "�Hola!", "The Spanish translation failed for this key: " + key);
+			Assert.AreEqual(translator.Translate(key, LocaleEnum.DE), "Hallo!", "The German translation failed for this key: " + key);
+		}
+	}
+}
