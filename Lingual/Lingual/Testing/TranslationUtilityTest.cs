512fe3bfe0010c38efe269fc863480a8860d0e12
Christian Lang <clang@student.neumont.edu>
2014-10-20 16:07:29 -0600

    added interpolation test and updated test cases

--- Lingual/Lingual/Testing/TranslationUtilityTest.cs
+++ Lingual/Lingual/Testing/TranslationUtilityTest.cs
@@ -13,11 +13,20 @@ namespace Lingual.Testing
 		private TranslationUtility translator = TranslationUtility.Instance;
 
 		[TestCase("login.hello")]
+		[TestCase("account.invalid.message")]
 		public void TestHelloKeyTranslation(string key)
 		{
 			Assert.AreEqual(translator.Translate(key), "Hello!", "The English translation failed for this key: " + key);
-			Assert.AreEqual(translator.Translate(key, LocaleEnum.ES), "�Hola!", "The Spanish translation failed for this key: " + key);
+			Assert.AreEqual(translator.Translate(key, LocaleEnum.ES), "¡Hola!", "The Spanish translation failed for this key: " + key);
 			Assert.AreEqual(translator.Translate(key, LocaleEnum.DE), "Hallo!", "The German translation failed for this key: " + key);
+		}
+
+
+		[TestCase("loggedin.user.day", "Billy Bob", "Tuesday")]
+		public void TestInterpolatedStrings(string key, string userName, string day)
+		{
+			Assert.AreEqual(translator.Translate(key, LocaleEnum.EN, userName, day), "Hello Billy Bob! Today is Tuesday", "English Translation Failed");
+			Assert.AreEqual(translator.Translate(key, LocaleEnum.DE, userName, day), "Hallo, Billy Bob. Heute ist Tuesday", "German Translation Failed");
 		}
 	}
 }
