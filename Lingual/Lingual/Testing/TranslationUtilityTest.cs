e9acc42b7f105af690b7c3f272dafd54775caa29
Christian Lang <clang@student.neumont.edu>
2014-10-21 14:34:56 -0600

    added more edge case testing and cleared up some testing

--- Lingual/Lingual/Testing/TranslationUtilityTest.cs
+++ Lingual/Lingual/Testing/TranslationUtilityTest.cs
@@ -13,20 +13,66 @@ namespace Lingual.Testing
 		private TranslationUtility translator = TranslationUtility.Instance;
 
 		[TestCase("login.hello")]
-		[TestCase("account.invalid.message")]
-		public void TestHelloKeyTranslation(string key)
+		
+		public void TestHelloKeyExcpectedToPassTranslation(string key)
 		{
-			Assert.AreEqual(translator.Translate(key), "Hello!", "The English translation failed for this key: " + key);
-			Assert.AreEqual(translator.Translate(key, LocaleEnum.ES), "¡Hola!", "The Spanish translation failed for this key: " + key);
-			Assert.AreEqual(translator.Translate(key, LocaleEnum.DE), "Hallo!", "The German translation failed for this key: " + key);
+			Assert.AreEqual("Hello!", translator.Translate(key), "The English translation failed for this key: " + key);
+			Assert.AreEqual( "¡Hola!", translator.Translate(key, LocaleEnum.ES), "The Spanish translation failed for this key: " + key);
+			Assert.AreEqual("Hallo!", translator.Translate(key, LocaleEnum.DE), "The German translation failed for this key: " + key);
+		}
+
+		[TestCase("")]
+		[TestCase("account.invalid.message")]
+		public void TestHelloKeyExpectedToFailTranslation(string key)
+		{
+			Assert.AreNotEqual("Hello!", translator.Translate(key), "English: these should not be equal!");
+			Assert.AreNotEqual("¡Hola!", translator.Translate(key, LocaleEnum.ES), "Spanish: these should not be equal!");
+			Assert.AreNotEqual("Hallo!", translator.Translate(key, LocaleEnum.DE), "German: these should not be equal!");
+		}
+
+		
+		[TestCase("loggedin.user.day", "Billy Bob", "Tuesday")]
+		public void TestAcceptedInterpolatedStrings(string key, string userName, string day)
+		{
+			Assert.AreEqual("Hello Billy Bob! Today is Tuesday", translator.Translate(key, LocaleEnum.EN, userName, day), "English Translation Failed");
+			Assert.AreEqual("Hallo, Billy Bob. Heute ist Tuesday", translator.Translate(key, LocaleEnum.DE, userName, day), "German Translation Failed");
+		}
+
+		[TestCase("login.hello")]
+		[TestCase("")]
+		public void TestExcpectedToFailInterpolatedStrings(string key)
+		{
+			Assert.AreNotEqual("Hello Billy Bob! Today is Tuesday", translator.Translate(key));
+			Assert.AreNotEqual("Hallo, Billy Bob. Heute ist Tuesday", translator.Translate(key));
+		}
+
+		[TestCase(0.0, "$0.00")]
+		[TestCase(10.0, "$10.00")]
+		[TestCase(100.0, "$100.00")]
+		[TestCase(10000000000.0, "$10,000,000,000.00")]
+		public void TestCurrencyValuesEn(double value, string answer)
+		{
+			Assert.AreEqual(answer, translator.TranslateCurrency(value));
 		}
 
 
-		[TestCase("loggedin.user.day", "Billy Bob", "Tuesday")]
-		public void TestInterpolatedStrings(string key, string userName, string day)
+		[TestCase(0.0, "0,00 €")]
+		[TestCase(10.0, "10,00 €")]
+		[TestCase(100.0, "100,00 €")]
+		[TestCase(10000000000.0, "10.000.000.000,00 €")]
+		public void TestCurrencyValuesEs(double value, string answer)
 		{
-			Assert.AreEqual(translator.Translate(key, LocaleEnum.EN, userName, day), "Hello Billy Bob! Today is Tuesday", "English Translation Failed");
-			Assert.AreEqual(translator.Translate(key, LocaleEnum.DE, userName, day), "Hallo, Billy Bob. Heute ist Tuesday", "German Translation Failed");
+			Assert.AreEqual(answer, translator.TranslateCurrency(value, LocaleEnum.ES));
+		}
+
+
+		[TestCase(0.0, "0,00 €")]
+		[TestCase(10.0, "10,00 €")]
+		[TestCase(100.0, "100,00 €")]
+		[TestCase(10000000000.0, "10.000.000.000,00 €")]
+		public void TestCurrencyValuesDe(double value, string answer)
+		{
+			Assert.AreEqual(answer, translator.TranslateCurrency(value, LocaleEnum.DE));
 		}
 	}
 }
