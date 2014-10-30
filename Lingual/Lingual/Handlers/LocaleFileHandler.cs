cf68be25edfd16bf68e2bb9d65ecdca944367fc0
BJ Neilsen <bj.neilsen@gmail.com>
2014-10-30 14:52:09 -0600

    Add methods to LocaleFileHandler for checking if a locale file exists

--- Lingual/Lingual/Handlers/LocaleFileHandler.cs
+++ Lingual/Lingual/Handlers/LocaleFileHandler.cs
@@ -9,7 +9,7 @@ namespace Lingual.Handlers
     public class LocaleFileHandler
     {
         private static readonly String ProjPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-        
+
         /// <summary>
         /// Gets the translation file for the specified locale
         /// </summary>
@@ -24,6 +24,27 @@ namespace Lingual.Handlers
                 contents = File.ReadAllText(localePath, Encoding.UTF8);
             }
             catch (DirectoryNotFoundException)
+
+        /// <summary>
+        /// Return bool indicating if the file exists for this locale.
+        /// </summary>
+        /// <param name="locale">Locale file to look for</param>
+        /// <returns>Bool indicating if the file exists for this locale</returns>
+        public static bool LocaleFileExists(Locale locale)
+        {
+            return File.Exists(FilePathForLocale(locale));
+        }
+
+        /// <summary>
+        /// Return file path for locale file to load.
+        /// </summary>
+        /// <param name="locale">Locale file to compute a path for.</param>
+        /// <returns>Path to file for locale</returns>
+        public static string FilePathForLocale(Locale locale)
+        {
+            return Path.Combine(ProjPath, "locale", locale.ToString().Replace('_', '-') + ".json");
+        }
+
             {
                 throw new LocaleFolderNotFoundException();
             }
