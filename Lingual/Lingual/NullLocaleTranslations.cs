ebf06262b5e0224cafe28e94ee3c98bf22a9ab4c
Christian Lang <clang@student.neumont.edu>
2014-10-31 14:17:51 -0600

    Fix #15, fix #14, fix #12

---
+++ Lingual/Lingual/NullLocaleTranslations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lingual
+{
+    public class NullLocaleTranslations : ILocaleTranslation
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <param name="value">Value.</param>
+        public void AddTranslation(string key, string value)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns><c>true</c>, if exists was keyed, <c>false</c> otherwise.</returns>
+        /// <param name="key">Key.</param>
+        public bool ContainsKey(string key)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns>The value.</returns>
+        /// <param name="key">Key.</param>
+        /// <param name="plurality">Plurality.</param>
+        public string GetValue(string key, Plurality? plurality = null)
+        {
+            return key;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns><c>true</c> if this instance has fallback locale; otherwise, <c>false</c>.</returns>
+        public bool HasFallbackLocale()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns><c>true</c> if this instance is translation dictionary empty; otherwise, <c>false</c>.</returns>
+        public bool IsTranslationDictionaryEmpty()
+        {
+            return true;
+        }
+    }
+}
